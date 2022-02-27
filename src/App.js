dd3c79021840b9cd14c69837aff519005e9227ed
Nguyen Thanh Dat <topthuthuat69@gmail.com>
2022-02-27 10:38:03 +0700

    tao du an

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="shadow-sm ..." >dsads</div>
+        <div className="shadow ..." >adsadas </div>
+        <div className="shadow-md ..." >dsad</div>
+        <div className="shadow-lg ..." > dsad</div>
+        <div className="shadow-xl ..." >dsada </div>
+        <div className="shadow-2xl ..." >dsada </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
