053a3eecd6ee1f486bf1f3242640a8f0a02168bf
Nguyen Thanh Dat <topthuthuat69@gmail.com>
2022-04-07 01:25:57 +0700

    load cum rap

--- src/pages/Home/HomeMenu/HomeMenu.js
+++ src/pages/Home/HomeMenu/HomeMenu.js
@@ -16,26 +16,69 @@ export default class Demo extends React.PureComponent {
         this.setState({ tabPosition: e.target.value });
     };
     componentDidMount() {
-        
+
     }
 
+    renderHeThongRap = () => {
+        return this.props.heThongRapChieu?.map((heThongRap, index) => {
+            let { tabPosition } = this.state;
+            return <TabPane tab={<img src={heThongRap.logo} className='rounded-full' width="50"></img>} key={index}>
+                <Tabs tabPosition={tabPosition}>
+                    {heThongRap.lstCumRap?.map((cumRap, index) => {
+                        return <TabPane tab={
+                            <div style={{ width: '300px', display: 'flex' }}>
+                                <img src='https://s3img.vcdn.vn/123phim/2021/01/bhd-star-bitexco-16105952137769.png' width="50" /> <br />
+                                <div className='text-left ml-2'>
+                                    {cumRap.tenCumRap}
+                                    <p className='text-red-200 ml-2'>Chi tiết</p>
+                                </div>
+                            </div>
+                        }
+                            key={index}>
+                            {/*Load phim tương ứng   */}
+                            {cumRap.danhSachPhim.slice(0, 3).map((phim, index) => {
+                                return <Fragment key={index}>
+                                    <div className='my-5' >
+                                        <div style={{ display: 'flex' }}>
+                                            <img style={{ height: 75, width: 75 }} src={phim.hinhAnh} alt={phim.tenPhim} onError={(e) => { 
+                                                e.target.onError = null;e.target.src = 'https://picsum.photos/75/75'; }}  />
 
+                                            <div className='ml-2'>
+                                                <h1 className='text-xl text-green-700'> {phim.tenPhim} </h1>
+                                                <p> {cumRap.diaChi} </p>
+                                                <div className='grid grid-cols-6 gap-6'>
+                                                    {phim.lstLichChieuTheoPhim?.slice(0, 12).map((lichChieu, index) => {
+                                                        return <NavLink className="text-xl text-green-400" to="/">
+                                                            {moment(lichChieu.ngayChieuGioChieu).format('hh:mm A')}
+                                                        </NavLink>
+                                                    })}
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                        <div>
+
+                                        </div>
+                                    </div>
+                                    <hr />
+                                </Fragment>
+                            })}
+                        </TabPane>
+
+                    })}
+
+                </Tabs>
+            </TabPane>
+        })
+    }
 
     render() {
-        console.log(this.props,'props123');
+        console.log(this.props, 'props123');
         const { tabPosition } = this.state;
         return (
             <>
                 <Tabs tabPosition={tabPosition}>
-                    <TabPane tab={<img src='https://picsum.photos/200' className='rounded-full' width="50"></img>} key="1">
-                        Content of Tab 1
-                    </TabPane>
-                    <TabPane tab={<img src='https://picsum.photos/200' className='rounded-full' width="50"></img>} key="2">
-                        Content of Tab 2
-                    </TabPane>
-                    <TabPane tab={<img src='https://picsum.photos/200' className='rounded-full' width="50"></img>} key="3">
-                        Content of Tab 3
-                    </TabPane>
+                    {this.renderHeThongRap()}
                 </Tabs>
             </>
         );
